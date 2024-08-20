390c903661fc20fe1bbddcf0056bfeb37d91e5df
Traianis <93998640+Traianis@users.noreply.github.com>
2024-08-20 10:57:24 +0300

    Camera_spate_telefon

--- main.py
+++ main.py
@@ -1,45 +1,45 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-st.title("Flux video live folosind HTML și JavaScript")
+st.title("Flux video live folosind HTML și JavaScript - Camera din spate")
 
 # Cod HTML și JavaScript pentru accesarea camerei și afișarea fluxului video
 html_code = """
-    <div>
-        <video id="video" width="100%" height="auto" autoplay></video>
+    <div style="text-align: center;">
+        <video id="video" width="100%" height="auto" autoplay style="border: 1px solid black;"></video>
+        <br>
         <button id="capture">Capturează imagine</button>
         <canvas id="canvas" style="display:none;"></canvas>
         <img id="captured-image" style="display:none;" alt="Captured Image"/>
     </div>
 
     <script>
-    // Accesăm camera
+    // Accesăm camera din spate
     var video = document.getElementById('video');
     var canvas = document.getElementById('canvas');
     var context = canvas.getContext('2d');
     var capturedImage = document.getElementById('captured-image');
 
-    // Inițializăm fluxul video
-    navigator.mediaDevices.getUserMedia({ video: true })
+    // Inițializăm fluxul video cu suport pentru camera din spate
+    navigator.mediaDevices.getUserMedia({ video: { facingMode: { exact: 'environment' } } })
         .then(function(stream) {
             video.srcObject = stream;
         })
         .catch(function(err) {
-            console.log("Eroare: " + err);
+            console.log("Eroare la accesarea camerei: " + err);
         });
 
     // Capturăm imaginea la apăsarea butonului
     document.getElementById('capture').addEventListener('click', function() {
         // Setăm dimensiunea canvas-ului la dimensiunile video-ului
-        //canvas.width = video.videoWidth;
-        //canvas.height = video.videoHeight;
-        
+        // canvas.width = video.videoWidth;
+        // canvas.height = video.videoHeight;
+
         // Test
         canvas.width = 550;
         canvas.height = 400;
-        
         context.drawImage(video, 0, 0, canvas.width, canvas.height);
-        
+
         // Convertim imaginea în data URL și o afișăm
         var dataURL = canvas.toDataURL('image/png');
         capturedImage.src = dataURL;
